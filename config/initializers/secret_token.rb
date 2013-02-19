2ae7ae1263856a2b9ad9ed7342141c5adeafd84b
Andrew Cranston <andrewtcranston@gmail.com>
2013-02-19 10:52:16 -0800

    initial install of rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SaveMeASeat::Application.config.secret_token = '24ef76daf1c1e241f55512e318ca42c86c54273954fbc7e0969ceb26235bcc072379d471f54a10b23e25f081366bcc614e456eebd602f9488bdf923bbe2dfe27'
